fd4a782b86ba24c2550e5eac1b38feb66ab7fa2c
ayushkalra01 <56130930+ayushkalra01@users.noreply.github.com>
2026-07-24 22:55:07 +0530

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+
+        int[] last = new int[26];
+
+        // Store last occurrence of every character
+        for (int i = 0; i < s.length(); i++) {
+            last[s.charAt(i) - 'a'] = i;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            end = Math.max(end, last[s.charAt(i) - 'a']);
+
+            // Current partition ends here
+            if (i == end) {
+                ans.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return ans;
+    }
+}
